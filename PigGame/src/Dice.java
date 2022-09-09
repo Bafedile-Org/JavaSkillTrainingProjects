59d2e93228d4bf31e85499cd36634ba62d88a6be
Bafedile <68484817+Bafedile@users.noreply.github.com>
2022-09-09 08:47:28 +0200

    initial commit

---
+++ PigGame/src/Dice.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Dimakatso Bafedile Sebatane
+ */
+public class Dice {
+    int roll(){
+        return (int)(Math.random()*6)+1;
+    }
+}
