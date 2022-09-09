cfc01108c3cd12f38f4bf651761f2cce1ce041a7
Bafedile <Ddimakatso30@gmail.com>
2022-09-09 15:02:06 +0200

    included the methods

--- ShapesClass/src/ShapesClass.java
+++ ShapesClass/src/ShapesClass.java
@@ -14,27 +14,19 @@ public class ShapesClass {
     }
 
     private void runMe() {
-        Shape circle = new Circle(10, "Circle");
-        System.out.printf(circle.toString());
+        Shape shapes[] = {new Circle(10, "Circle"),
+            new Cylinder(10, "Cylinder", 5),
+            new Rectangle(10, 5, "Rectangle"),
+            new Square(10, "Square"),
+            new Cube(200, "Cube")};
 
-        System.out.println("-------------------------------------------------------");
-
-        Shape cylinder = new Cylinder(10, "Cylinder", 5);
-        System.out.printf(cylinder.toString());
-        System.out.println("-------------------------------------------------------");
-
-        Shape rectangle = new Rectangle(10, 5, "Rectangle");
-        System.out.printf(rectangle.toString());
-        System.out.println("-------------------------------------------------------");
-
-        Shape square = new Square(10, "Square");
-        System.out.printf(square.toString());
-
-        System.out.println("-------------------------------------------------------");
-
-        Shape cube = new Cube(10, "Cube");
-        System.out.printf(cube.toString());
-
+        printShapes(shapes);
+    }
+    private void printShapes(Shape shapes[]){
+        for(Shape shape: shapes){
+            System.out.println(shape.toString()+""
+                    + "\n=============================================");
+        }
     }
 
 }
