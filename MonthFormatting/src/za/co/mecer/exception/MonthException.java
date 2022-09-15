84443a3834d8d354bdaa97c1e8f2bddd3136aef4
Bafedile <Ddimakatso30@gmail.com>
2022-09-15 15:17:55 +0200

    initial commit

---
+++ MonthFormatting/src/za/co/mecer/exception/MonthException.java
@@ -0,0 +1,12 @@
+package za.co.mecer.exception;
+
+/**
+ *
+ * @author Dimakatso Sebatane
+ */
+public class MonthException extends Exception {
+
+    public MonthException(String msg) {
+        super(msg);
+    }
+}
