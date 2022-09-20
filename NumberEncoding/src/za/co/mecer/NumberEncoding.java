902f1183f4b742540dfc142fb2aba549a691420e
Bafedile <Ddimakatso30@gmail.com>
2022-09-20 09:55:51 +0200

    Added Thandeka's version of the code

--- NumberEncoding/src/za/co/mecer/NumberEncoding.java
+++ NumberEncoding/src/za/co/mecer/NumberEncoding.java
@@ -11,8 +11,10 @@ public class NumberEncoding {
     /**
      * @param args the command line arguments
      */
+    private final char[] symbols = {'*', 'B', 'E', 'A', '@', 'F', 'K', '%', 'R', 'M'};
+
     public static void main(String[] args) {
-        new NumberEncoding().run();
+        new NumberEncoding().runThandeksCode();
     }
 
     private void run() {
@@ -32,7 +34,7 @@ public class NumberEncoding {
     }
 
     private void encode(StringBuilder sb, String numString, char ch) {
-        char[] symbols = {'*', 'B', 'E', 'A', '@', 'F', 'K', '%', 'R', 'M'};
+
         sb.append(ch);
         for (int i = 1; i < numString.length() - 1; i++) {
             sb.append(symbols[Integer.parseInt("" + numString.charAt(i))]);
@@ -45,6 +47,41 @@ public class NumberEncoding {
         Scanner input = new Scanner(System.in);
         System.out.print("Please enter a sequence of numbers: ");
         return input.next();
+    }
+
+    /**
+     * Thandeka's Code
+     *
+     * @param number
+     * @return
+     */
+    private void runThandeksCode() {
+        String numString = getString();
+
+        System.out.printf("Encoded Numerals: %s%n", generateSymbol(numString));
+    }
+
+    private String generateSymbol(String number) {
+        StringBuilder sb = new StringBuilder(number);
+        int myLastValue = number.length() - 1;
+        for (int i = 0; i < number.length(); i++) {
+            char ch = symbols[Character.getNumericValue(number.charAt(i))];
+            sb.setCharAt(i, ch);
+        }
+
+        if (Character.getNumericValue(number.charAt(myLastValue)) == 0) {
+            sb.setCharAt(myLastValue, '#');
+        } else if (Character.getNumericValue(number.charAt(0)) % 2 != 0
+                && Character.getNumericValue(number.charAt(myLastValue)) % 2 != 0) {
+            sb.setCharAt(0, 'X');
+            sb.setCharAt(myLastValue, 'X');
+
+        } else {
+            sb.setCharAt(0, '$');
+            sb.setCharAt(myLastValue, '$');
+        }
+
+        return sb.toString();
     }
 
 }
