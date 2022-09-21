9a914dd25f25eaecbc5a22040a69075200f4cbd5
Bafedile <Ddimakatso30@gmail.com>
2022-09-21 09:45:36 +0200

    changed Class name from Card to Cards

--- MemoryMatchingGame/src/za/co/mecer/Cards/Cards.java
+++ MemoryMatchingGame/src/za/co/mecer/Cards/Cards.java
@@ -4,14 +4,18 @@ package za.co.mecer.Cards;
  *
  * @author Dimakatso Sebatane
  */
-public class Card {
+public class Cards {
 
     private int value;
     private String face;
 
-    public Card(String face, int value) {
+    public Cards(String face, int value) {
         this.setFace(face);
         this.value = value;
+    }
+
+    public Cards() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
     public void setFace(String face) {
