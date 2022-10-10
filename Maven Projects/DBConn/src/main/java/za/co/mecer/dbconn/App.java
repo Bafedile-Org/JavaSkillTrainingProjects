16909964c3b5505490acaa9451498009f2978d06
Bafedile <Ddimakatso30@gmail.com>
2022-10-10 13:24:02 +0200

    initial commit

---
+++ Maven Projects/DBConn/src/main/java/za/co/mecer/dbconn/App.java
@@ -0,0 +1,21 @@
+package za.co.mecer.dbconn;
+
+/**
+ *
+ * @author Dimakatso Sebatane
+ */
+/**
+ * Hello world!
+ *
+ */
+public class App {
+
+    public static void main(String[] args) {
+        new App().run();
+
+    }
+
+    private void run() {
+        System.out.printf("Hello There!%nWelcome to Maven HELL%n");
+    }
+}
