7108d177564b869bbbc94c6295a51c07aa2fb7c9
Bafedile <Ddimakatso30@gmail.com>
2022-10-10 15:36:33 +0200

    Update DatabaseConnection.java

--- Maven Projects/LibrarySystem/src/main/java/za/co/mecer/dbconnection/DatabaseConnection.java
+++ Maven Projects/LibrarySystem/src/main/java/za/co/mecer/dbconnection/DatabaseConnection.java
@@ -1,7 +1,12 @@
 package za.co.mecer.dbconnection;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.Properties;
 
 /**
  *
@@ -13,7 +18,20 @@ public class DatabaseConnection {
     private static PreparedStatement preparedStatement = null;
     private static Connection conn = null;
 
-    private DatabaseConnection() {
+    private DatabaseConnection() throws SQLException {
 
+    }
+
+    private static void connect() throws SQLException {
+        String user, url, pass;
+        try (InputStream stream = new FileInputStream("database.properties")) {
+            Properties properties = new Properties();
+            properties.load(stream);
+            url = properties.getProperty("url");
+            user = properties.getProperty("username");
+            pass = properties.getProperty("password");
+        } catch (IOException iox) {
+            System.out.printf("Error: %s%n", iox.getMessage());
+        }
     }
 }
